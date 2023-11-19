77e35599d51755440976200ebd493c0062c1bf10
ERFAN pournasiri <erfanpournasiri71@gmail.com>
2023-11-19 19:59:59 +0330

    Create card and transction part

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import styles from '../ui/dashboard/dashboard.module.css'
+import RightBar from '../ui/dashboard/rightbar/RightBar'
+import TranSection from '../ui/dashboard/transection/TranSection'
+import Chart from '../ui/dashboard/chart/Chart'
+import Card from '../ui/dashboard/card/Card'
+
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cart}>
+          <Card/>
+          <Card/>
+          <Card/>
+        </div>
+        <div className={""}>
+          <TranSection/>
+        </div>
+        <div>
+          <Chart/>
+          </div>
+      </div>
+      <div className={styles.side}>
+        <RightBar/>
+      </div>
+    </div>
+
   )
 }
 
